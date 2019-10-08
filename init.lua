5e6d90481f1dbaac177249b529ad949c8b46183b
Jared Taylor <jared.xyz@mailbox.org>
2019-10-07 22:25:08 -0400

    Added ability to revert to normal

--- init.lua
+++ init.lua
@@ -1,5 +1,38 @@
 print("Will run at load time!")
 
-minetest.register_node("testmod:node", {
-	description = "This is a node"
+playerSpeed = 1
+playerGravity = 1
+
+minetest.register_chatcommand("run", {
+    func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        player:set_physics_override({
+			speed = 2,
+			gravity = playerGravity
+		})
+		playerSpeed = 2
+    end,
+})
+
+minetest.register_chatcommand("antigravity", {
+    func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        player:set_physics_override({
+			speed = playerSpeed,
+			gravity = 0.1
+		})
+		playerGravity = 0.1
+    end,
+})
+
+minetest.register_chatcommand("revert", {
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+        player:set_physics_override({
+			speed = 1,
+			gravity = 1
+		})
+		playerGravity = 1
+		playerSpeed = 1
+    end,
 })
